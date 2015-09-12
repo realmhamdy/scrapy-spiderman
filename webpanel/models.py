f3f26399efea95cf7b1447fd0d8bd442df6e56f9
Mohammed Hamdy <mohammed,hamdy777@gmail.com>
2015-09-12 21:49:56 +0200

    Added basic spider model

--- webpanel/models.py
+++ webpanel/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+
+
+class Spider(models.Model):
+
+    name = models.CharField(max_length=255, null=False, blank=False)
+    running = models.BooleanField(default=False, null=True, blank=True)
+    runtime = models.DateTimeField(null=True, blank=True)
