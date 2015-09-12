c3e44ffeba00cb91fe538ff87f1d0a564e74f99a
Mohammed Hamdy <mohammed,hamdy777@gmail.com>
2015-09-12 11:38:50 +0200

    Added Vagrant setup

---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo apt-get install -y python-pip python-dev libxml2-dev libxslt-dev zlib1g-dev python-psycopg2 postgresql \
+    postgresql-contrib
+pushd /vagrant
+sudo pip install -r requirements.txt
+sudo python manage.py runserver 0.0.0.0:80
