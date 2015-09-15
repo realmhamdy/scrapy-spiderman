e9cbe8d476e4653c29476218d5da9f4f4cd0ec3b
Mohammed Hamdy <mohammed,hamdy777@gmail.com>
2015-09-15 04:15:19 +0200

    Added ability for a Spider to reference it's item model

--- webpanel/models/app_models.py
+++ webpanel/models/app_models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.contenttypes.models import ContentType
 
 
 class Spider(models.Model):
@@ -8,9 +9,15 @@ class Spider(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False)
     running = models.BooleanField(default=False)
     runtime = models.DateTimeField(null=True, blank=True, help_text=_("The last time this spider was started"))
+    item_model_name = models.CharField(max_length=128, blank=False, null=False,
+                                       help_text=_("Used to refer to the item model for this spider"))
 
     class Meta:
         app_label = "webpanel"
+
+    @property
+    def item_model(self):
+        return ContentType.objects.get_by_natural_key("webpanel", self.item_model_name).model_class()
 
     def __unicode__(self):
         return self.name
